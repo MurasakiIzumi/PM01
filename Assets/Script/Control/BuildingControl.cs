3723e91df229aee27e848a072a3ee60e2339eb82
MurasakiIzumi <dova879537478@gmail.com>
2024-04-12 16:52:49 +0900

    仮ステージ建設

---
+++ Assets/Script/Control/BuildingControl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingControl : MonoBehaviour
+{
+    [HideInInspector] private int hp;
+    [HideInInspector] private float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 10;
+        speed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(hp<=0)
+        {
+            transform.position -= new Vector3(0, 1.0f, 0) * speed * Time.deltaTime;
+        }
+
+        if(transform.position.y<-5.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            hp -= other.GetComponent<BulletControl>().damage;
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "Rocket")
+        {
+            hp -= other.GetComponent<RocketContrl>().damage;
+            other.GetComponent<RocketContrl>().DestroySelf();
+        }
+
+        if (other.gameObject.tag == "Laser")
+        {
+            hp -= other.GetComponent<LaserControl>().damage;
+        }
+
+    }
+}
